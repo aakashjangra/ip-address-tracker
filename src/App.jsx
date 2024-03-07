e38bf2534fe81004ae21c1f95732917ce6d33937
Akash Jangra <aj.akashjangra@gmail.com>
2024-03-07 12:47:32 +0530

    api changed and data rendering redefined acc to new api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import axios from 'axios';
 import Map from './components/Map';
@@ -8,13 +8,23 @@ import Notification from './components/Notification';
 
 function App() {
   const [inputValue, setInputValue] = useState('');
-
+  const isIpAddress = (input) => {
+    if (input && !isNaN(input[0])) return true;
+  }
   const getData = async (input) => {
     try {
-      const res = await axios.get(`http://ip-api.com/json/${input}?fields=status,message,country,city,zip,lat,lon,timezone,isp,org,query`);
-      if (res.status === 200 && res.data.status === "success"){
+      const URL = import.meta.env.VITE_API_URL;
+      let res;
+      if (isIpAddress(input)) {
+        res = await axios.get(`${URL}&ipAddress=${input}`);
+      } else {
+        res = await axios.get(`${URL}&domain=${input}`);
+      }
+      console.log('res is - ', res, res.data);
+
+      if (res.status === 200) {
         return res.data;
-      } 
+      }
     } catch (error) {
       console.log('error while fetching ip details :/')
     }
@@ -23,12 +33,27 @@ function App() {
   }
   const { error, data, isFetching, refetch } = useQuery({
     queryKey: ['details'],
-    queryFn: () => getData(inputValue), 
+    queryFn: () => getData(inputValue),
+    retry: false,
   })
 
   const trackIP = () => {
-    refetch(); 
+    refetch();
   }
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        console.log('enter key pressed')
+        trackIP();
+      }
+    });
+
+    return () => {
+      document.body.removeEventListener('enter');
+    }
+  }, [])
+
 
   return (
     <div className="relative w-screen h-screen bg-blue-50">
@@ -58,38 +83,44 @@ function App() {
         <div className="result bg-white w-[83%] grid sm:grid-cols-4 p-6 gap-4 rounded-lg overflow-wrap-anywhere">
           <div className="ip-address sm:border-r-2 flex flex-col items-center sm:block">
             <h3 className="title text-xs font-semibold text-slate-400 tracking-widest">IP/MAC ADDRESS</h3>
-            <h4 className="data font-bold text-lg">{data?.query}</h4>
+            <h4 className="data font-bold text-lg">{data?.ip}</h4>
           </div>
           <div className="location sm:border-r-2 flex flex-col items-center sm:block">
             <h3 className="title text-xs font-semibold text-slate-400 tracking-widest">LOCATION</h3>
-            <h4 className="data font-bold text-lg text-balance">{data?.city}, {data?.country}
+            <h4 className="data font-bold text-lg text-balance">{data?.location?.city}
+              {data?.location?.city && data?.location?.country && (
+                ','
+              )}
+              {data?.location?.country}
               <br />
-              {data?.zip}
+              {data?.location?.postalCode}
             </h4>
           </div>
           <div className="timezone sm:border-r-2 flex flex-col items-center sm:block">
             <h3 className="title text-xs font-semibold text-slate-400 tracking-widest">TIMEZONE</h3>
-            <h4 className="data font-bold text-lg">{data?.timezone}</h4>
+            <h4 className="data font-bold text-lg">{data?.location?.timezone}</h4>
           </div>
           <div className="isp flex flex-col items-center sm:block">
             <h3 className="title text-xs font-semibold text-slate-400 tracking-widest">ISP</h3>
             <h4 className="data font-bold text-lg">
               {data?.isp}
+              <br />
+              {data?.as?.name}
             </h4>
           </div>
         </div>
       </div>
-      <Map styles={'absolute bottom-0 h-[70%] z-10 w-full map-offset sm:map-center'} coordinates={{ lat: data?.lat ? data?.lat: 45, long: data?.lon? data?.lon: 89 }} />
+      <Map styles={'absolute bottom-0 h-[70%] z-10 w-full map-offset sm:map-center'} coordinates={{ lat: data?.lat ? data?.lat : 45, long: data?.lng ? data?.lng : 89 }} />
 
 
-          <Loading styles={`${isFetching == false? 'hidden': ''} fixed top-0 left-0 w-screen h-screen flex items-center justify-center z-50 bg-white/60`} />
+      <Loading styles={`${isFetching == false ? 'hidden' : ''} fixed top-0 left-0 w-screen h-screen flex items-center justify-center z-50 bg-white/60`} />
 
-          {
-            error && (
-              <Notification message="Invalid IP/Domain"/>
-            )
-          }
-          
+      {
+        error && (
+          <Notification message="Invalid IP/Domain" />
+        )
+      }
+
     </div>
   )
 }
